a38c705cc1581f701bb4fff2a2ad7f3d7afe097f
SilverFangCoding <105740151+SilverFangCoding@users.noreply.github.com>
2025-11-04 10:14:09 +0000

    Mortgage payment list
    Calculate outstanding mortgage with interest rate and deduct the monthly payments, uses a list to log all the changes in the outstanding mortgage. Output is a print of such list.

---
+++ mortgagelist.py
@@ -0,0 +1,13 @@
+rate = 0.05
+payment = 200
+mortgage = 1000
+outstanding = []
+
+outstanding = outstanding + [mortgage]
+
+while (mortgage > 0):
+
+    interest = mortgage * rate / 12
+    mortgage = mortgage + interest - payment
+    outstanding = outstanding + [mortgage]
+print ('Outstanding mortgage, month by month:', outstanding)
